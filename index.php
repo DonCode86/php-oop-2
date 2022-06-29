9a0023ea8240d66b5767c3b73f5996539cb968bc
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-06-29 12:45:30 +0200

    fix correzione mattina

--- index.php
+++ index.php
@@ -7,18 +7,18 @@ require_once __DIR__ . '/classes/CreditCard.php';
 $food = new Food('Croccantini', 10.20, 10, 'gatto', '2022-10-10');
 
 
-$user = new User('Samuele', 'Madrigali', 'samuele.madrigali@gmail.com');
-$registered_user = new RegisteredUser('Samantha', 'Madrigali', 'samantha.madrigali@gmail.com');
+$user = new User('Daniele', 'Ciccarelli', 'daniele.ciccarelli@gmail.com');
+$registered_user = new RegisteredUser('Erika', 'Ciccarelli', 'erika.ciccarelli@gmail.com');
 
 try {
-    $visa = new CreditCard('4000 4000 4000 4000', '2022-10-10', 'visa', 'Samuele', 'Madrigali', 123);
+    $visa = new CreditCard('0000 0000 0000 0000', '2022-10-10', 'visa', 'Daniele', 'Ciccarelli', 123);
     $user->setCreditCard($visa);
 } catch (Exception $e) {
     echo $e->getMessage();
 }
 
 try {
-    $amex = new CreditCard('1111 1111 1111 1111', '2022-01-10', 'amex', 'Samuele', 'Madrigali', 123);
+    $amex = new CreditCard('1111 1111 1111 1111', '2022-01-10', 'mastercard', 'Daniele', 'Ciccarelli', 123);
     $registered_user->setCreditCard($amex);
 } catch (Exception $e) {
     echo $e->getMessage();
